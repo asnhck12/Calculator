864747e93090496568421684d58bccf07caa54e4
AH LT <ahussain93@live.co.uk>
2022-12-08 16:31:10 +0000

    Added functions to the number buttons & text field

--- Calculator.js
+++ Calculator.js
@@ -1,7 +1,20 @@
+const display = document.getElementById("displayBox");
+const clear = document.getElementById("clearButton");
+const numbers = document.getElementsByClassName("numberButtons");
+const operator = document.getElementsByClassName("operatorButtons");
 
-var a = prompt("Enter the first number: ");
-var operation = prompt("Enter the operation: ");
-var b = prompt("Enter the second number: ");
+/* For testing the calculator in console   */
+// var a = prompt("Enter the first number: ");
+// var operation = prompt("Enter the operation: ");
+// var b = prompt("Enter the second number: ");
+
+
+//Added functions to the numbered buttons and the text field that would display the numbers selected
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener('click', function() {
+    display.value += this.value});
+};
+
 
 
 function operate() {
